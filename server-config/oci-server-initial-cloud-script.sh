a58b691143335ee9b47ba121589346363d93a349
Mariusz Antonik <mantonik@gmail.com>
2022-01-03 14:25:18 -0500

    Add list of files to download

--- server-config/oci-server-initial-cloud-script.sh
+++ server-config/oci-server-initial-cloud-script.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # script will perform inital configuration and download setup file
+#
+# Version 
+# 1.1 - add list of files to download
+#
 
 mkdir bin 
 cd bin 
 sudo dnf install -y wget 
+cd bin 
+
+#download instalation files 
+wget https://github.com/mantonik/oci-always-free-high-availability/blob/main/server-config/bin/02.server-instalation-script-app.sh
+
+chmod 750 *.sh
+
 
