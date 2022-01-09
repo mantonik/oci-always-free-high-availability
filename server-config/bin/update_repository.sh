24024e617c8f4df50813b42c074f1756ef276404
Mariusz Antonik <mantonik@gmail.com>
2022-01-08 23:41:30 -0500

    update

--- server-config/bin/update_repository.sh
+++ server-config/bin/update_repository.sh
@@ -8,11 +8,14 @@ export REPODIR=${HOME}/repository/${REPO}
 export https_proxy=http://10.10.1.11:3128;
 export http_proxy=http://10.10.1.11:3128;
 
+# Delete repo folder
+rm -rf ${REPODIR}
+mkdir -p ${REPODIR}
 cd ${REPODIR}
-rm -rf * 
+# upload repo file
 wget https://github.com/mantonik/oci-always-free-high-availability/archive/refs/heads/${REPO}.zip
-rm -rf oci-always-free-high-availability-${REPO}
 unzip ${REPO}.zip
+# echo copy to home
 cp -a oci-always-free-high-availability-${REPO}/server-config/* ${HOME}/
 cd ${HOME}
 ls -l
