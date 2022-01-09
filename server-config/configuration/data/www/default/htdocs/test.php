842b9dda823d54cc985c005af668b0c55ad41542
Mariusz Antonik <mantonik@gmail.com>
2022-01-09 15:50:58 -0500

    update

--- server-config/configuration/data/www/default/htdocs/test.php
+++ server-config/configuration/data/www/default/htdocs/test.php
@@ -1,10 +1,15 @@
-------------------------------------------------------------
-This is html text, below is a hostname value from gethostname().
-if you see it below then nginx and php are working correctly
-<?php
-echo "";
-echo "Site is running on host:";
-echo gethostname(); // may output e.g,: sandie
-echo ""
-?>
-------------------------------------------------------------
+<html>
+  <body>
+    <hr>
+    <br>This is html text, below is a hostname value from gethostname().
+    <br>if you see it below then nginx and php are working correctly
+    <?php
+    echo "<br>";
+    echo "<br>Site is running on host:";
+    echo "<br>".gethostname(); // may output e.g,: sandie
+    echo ""
+    ?>
+    <hr>
+
+  </body>
+</html>
