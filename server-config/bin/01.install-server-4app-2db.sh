4984a16448dec3b299d5452f67ea0cc01894be3d
Mariusz Antonik <mantonik@gmail.com>
2022-01-08 23:50:13 -0500

    add version message

--- server-config/bin/01.install-server-4app-2db.sh
+++ server-config/bin/01.install-server-4app-2db.sh
@@ -4,6 +4,7 @@
 # check if user is root
 
 #Run main instalation on each server 
+version=1.1
 
 echo "Run instalation script"
 sudo /home/opc/bin/02.server-instalation-script-app.sh
@@ -15,3 +16,6 @@ if [ "$HOSTNAME" == *"app2"* ] || [ "$HOSTNAME" == *"app4"* ] ; then
 
 fi
 
+echo "---------------------------"
+echo "Version: ${version}"
+echo "---------------------------"
