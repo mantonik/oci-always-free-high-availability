d27fe4357c57eff1c150c9404af0ea820a3c4d39
Mariusz Antonik <mantonik@gmail.com>
2022-01-09 00:24:56 -0500

    add alias

--- server-config/configuration/etc/profile.d/custom.sh
+++ server-config/configuration/etc/profile.d/custom.sh
@@ -1,4 +1,9 @@
 #Custom Profile file
+# 1.0 initial version 
+# 1.1 add alias
+###################
+version=1.1
+
 export PS1='\u@\h:\w\n#'
 export PATH=$PATH:$HOME/bin:/home/opc/bin
 export http_proxy=http://10.10.1.11:3128/
@@ -10,6 +15,8 @@ export no_proxy=localhost,127.0.0.1
 #umask=022 #default
 umask 0027
 
+#Aliases
 alias rsync_server="sudo /home/opc/bin/rsync_server.sh"
 
 # END
+
