90e09d55a90a63861c7eb07fcff50328acc967bd
Mariusz Antonik <mantonik@gmail.com>
2022-01-12 23:33:37 -0500

    update

--- server-config/configuration/etc/rc.d/init.d/mount_share.sh
+++ server-config/configuration/etc/rc.d/init.d/mount_share.sh
@@ -10,8 +10,8 @@ umount -lf /mnt/share_app4
 #Mount share 
 
 
-mount demoapp1:/share //mnt/share_app1
-mount demoapp2:/share //mnt/share_app2
-mount demoapp3:/share //mnt/share_app3
-mount demoapp4:/share //mnt/share_app4
+mount demoapp1:/share /mnt/share_app1
+mount demoapp2:/share /mnt/share_app2
+mount demoapp3:/share /mnt/share_app3
+mount demoapp4:/share /mnt/share_app4
 df -k
